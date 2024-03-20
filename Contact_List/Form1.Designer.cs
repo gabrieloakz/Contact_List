afc1cb7b5e6bcf9c60023f49297190689b5f50a4
Gabriel Carvalho <53023@PC03D308.aebescola.local>
2024-03-20 15:13:55 +0000

    version 1.0

--- Contact_List/Form1.Designer.cs
+++ Contact_List/Form1.Designer.cs
@@ -28,66 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.labelContactList = new System.Windows.Forms.Label();
-            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
-            this.AddButton = new System.Windows.Forms.ToolStripLabel();
-            this.toolStrip1.SuspendLayout();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnAbrirLista = new System.Windows.Forms.Button();
+            this.listBoxContatos = new System.Windows.Forms.ListBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // labelContactList
             // 
             this.labelContactList.AutoSize = true;
-            this.labelContactList.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.labelContactList.BackColor = System.Drawing.SystemColors.Window;
             this.labelContactList.Font = new System.Drawing.Font("Arial Rounded MT Bold", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelContactList.Location = new System.Drawing.Point(83, 9);
+            this.labelContactList.Location = new System.Drawing.Point(69, 9);
             this.labelContactList.Name = "labelContactList";
             this.labelContactList.Size = new System.Drawing.Size(203, 37);
             this.labelContactList.TabIndex = 1;
             this.labelContactList.Text = "Contact List";
             // 
-            // toolStrip1
+            // btnAdicionar
             // 
-            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
-            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripLabel1,
-            this.AddButton});
-            this.toolStrip1.Location = new System.Drawing.Point(289, 21);
-            this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(59, 25);
-            this.toolStrip1.TabIndex = 4;
-            this.toolStrip1.Text = "toolStrip1";
+            this.btnAdicionar.Location = new System.Drawing.Point(45, 529);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(75, 23);
+            this.btnAdicionar.TabIndex = 2;
+            this.btnAdicionar.Text = "Adicionar";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
             // 
-            // toolStripLabel1
+            // btnSalvar
             // 
-            this.toolStripLabel1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripLabel1.Name = "toolStripLabel1";
-            this.toolStripLabel1.Size = new System.Drawing.Size(0, 22);
-            this.toolStripLabel1.Text = "toolStripLabel1";
+            this.btnSalvar.Location = new System.Drawing.Point(207, 529);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.TabIndex = 3;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
             // 
-            // AddButton
+            // btnAbrirLista
             // 
-            this.AddButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.AddButton.Image = ((System.Drawing.Image)(resources.GetObject("AddButton.Image")));
-            this.AddButton.Name = "AddButton";
-            this.AddButton.Size = new System.Drawing.Size(16, 22);
-            this.AddButton.Text = "toolStripLabel2";
+            this.btnAbrirLista.Location = new System.Drawing.Point(126, 529);
+            this.btnAbrirLista.Name = "btnAbrirLista";
+            this.btnAbrirLista.Size = new System.Drawing.Size(75, 23);
+            this.btnAbrirLista.TabIndex = 4;
+            this.btnAbrirLista.Text = "Abrir";
+            this.btnAbrirLista.UseVisualStyleBackColor = true;
+            this.btnAbrirLista.Click += new System.EventHandler(this.btnAbrirLista_Click);
+            // 
+            // listBoxContatos
+            // 
+            this.listBoxContatos.FormattingEnabled = true;
+            this.listBoxContatos.Location = new System.Drawing.Point(56, 49);
+            this.listBoxContatos.Name = "listBoxContatos";
+            this.listBoxContatos.Size = new System.Drawing.Size(226, 212);
+            this.listBoxContatos.TabIndex = 5;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(86, 318);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 6;
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Location = new System.Drawing.Point(86, 344);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(100, 20);
+            this.txtTelefone.TabIndex = 7;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(86, 370);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtEmail.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 321);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Nome:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 377);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Email:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 351);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Telefone:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(357, 450);
-            this.Controls.Add(this.toolStrip1);
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(357, 564);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtTelefone);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.listBoxContatos);
+            this.Controls.Add(this.btnAbrirLista);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.btnAdicionar);
             this.Controls.Add(this.labelContactList);
             this.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.IsMdiContainer = true;
             this.Name = "Form1";
             this.Text = "Form1";
-            this.toolStrip1.ResumeLayout(false);
-            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -96,9 +166,16 @@
         #endregion
 
         private System.Windows.Forms.Label labelContactList;
-        private System.Windows.Forms.ToolStrip toolStrip1;
-        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
-        private System.Windows.Forms.ToolStripLabel AddButton;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnAbrirLista;
+        private System.Windows.Forms.ListBox listBoxContatos;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtTelefone;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Contact_List/Form1.cs
+++ Contact_List/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,89 @@ namespace Contact_List
 {
     public partial class Form1 : Form
     {
+        private List<Contato> contatos = new List<Contato>();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            // Adicionar um novo contato à lista
+            Contato novoContato = new Contato
+            {
+                Nome = txtNome.Text,
+                Telefone = txtTelefone.Text,
+                Email = txtEmail.Text
+            };
+            contatos.Add(novoContato);
+
+            // Atualizar a ListBox com o novo contato
+            listBoxContatos.Items.Add(novoContato);
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            // Salvar a lista de contatos em um arquivo
+            try
+            {
+                using (StreamWriter writer = new StreamWriter("contatos.txt"))
+                {
+                    foreach (Contato contato in contatos)
+                    {
+                        writer.WriteLine($"{contato.Nome},{contato.Telefone},{contato.Email}");
+                    }
+                }
+                MessageBox.Show("Contatos salvos com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao salvar contatos: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnAbrirLista_Click(object sender, EventArgs e)
+        {
+            // Abrir a lista de contatos de um ficheiro
+            try
+            {
+                using (StreamReader reader = new StreamReader("contatos.txt"))
+                {
+                    string linha;
+                    while ((linha = reader.ReadLine()) != null)
+                    {
+                        string[] dados = linha.Split(',');
+                        Contato contato = new Contato
+                        {
+                            Nome = dados[0],
+                            Telefone = dados[1],
+                            Email = dados[2]
+                        };
+                        contatos.Add(contato);
+                        listBoxContatos.Items.Add(contato);
+                    }
+                }
+                MessageBox.Show("Contatos carregados com sucesso!");
+            }
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show("Arquivo de contatos não encontrado. A lista de contatos está vazia.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao carregar contatos: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        public class Contato
+        {
+            public string Nome { get; set; }
+            public string Telefone { get; set; }
+            public string Email { get; set; }
+
+            public override string ToString()
+            {
+                return $"{Nome} - {Telefone} - {Email}";
+            }
         }
     }
 }
